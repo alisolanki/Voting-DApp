9102452a2388e3f159d5d4303876e4eb95f26b30
Ali Solanki <alisolanki123@gmail.com>
2021-04-10 19:51:46 +0530

    first

---
+++ src/js/app.js
@@ -0,0 +1,58 @@
+var contractABI = [
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "candidates",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "name",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "votes",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "candidatesCount",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  }
+];
+var contractAddress = "0xbdBA48c8939e35154646692D12D2741e234991AB";
+var web3 = new Web3('http://localhost:7545');
+var voting = new web3.eth.Contract(contractABI, contractAddress);
+
+console.log(voting);
+
+web3.eth.getAccounts().then(console.log);

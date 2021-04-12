03ac76d03cd8c5237b1ab3ef515c71c9df06997f
Ali Solanki <alisolanki123@gmail.com>
2021-04-12 21:35:28 +0530

    candidatesName

--- src/js/app.js
+++ src/js/app.js
@@ -49,10 +49,22 @@ var contractABI = [
     "constant": true
   }
 ];
-var contractAddress = "0xbdBA48c8939e35154646692D12D2741e234991AB";
+var contractAddress = "0xD98c150ca4339664eC66f932bE68043Ad5F9703f";
 var web3 = new Web3('http://localhost:7545');
 var voting = new web3.eth.Contract(contractABI, contractAddress);
 
 console.log(voting);
 
 web3.eth.getAccounts().then(console.log);
+
+document.addEventListener('DOMContentLoaded', async () => {
+  var candidatesCount = await voting.methods.candidatesCount().call();
+
+  var x = "", i;
+  for (i = 1; i <= candidatesCount; i++) {
+    var candidateName = await voting.methods.candidates(i).call();
+    x = x + "<h2>" + candidateName.name + "</h>";
+  }
+
+  document.getElementById('candidate').innerHTML = x;
+})
